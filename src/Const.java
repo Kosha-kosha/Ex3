4a3e6e51cd3a8b2f4924c456a672fc7b5a265269
Kosha-kosha <70766513+Kosha-kosha@users.noreply.github.com>
2021-06-18 15:49:04 +0300

    Delete src directory

--- src/Const.java
+++
@@ -1,9 +0,0 @@
-public class Const {
-    public static final String USER_TABLE = "users";
-
-    public static final String USERS_ID = "idUsers";
-    public static final String USERS_LOGIN = "login";
-    public static final String USERS_PASS = "password";
-
-
-}
